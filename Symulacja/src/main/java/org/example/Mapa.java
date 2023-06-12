fefeedb822f11576c16492e906b72fbf7ab7338f
adumk <krawczyk.adam.2004@gmail.com>
2023-06-12 21:07:16 +0200

    Zapis do pliku

--- Symulacja/src/main/java/org/example/Mapa.java
+++ Symulacja/src/main/java/org/example/Mapa.java
@@ -1,14 +1,18 @@
 package org.example;
 
+import java.io.*;
 import java.util.ArrayList;
 
 class Mapa {
-    private static int nr=1;
-    private int x=0;
+    private static int nr = 1;
+    private int x = 0;
     private int rzedy, kolumny;
     NazwaJednostki[] jednostkiKolumny;
     private String nazwa;
-    ArrayList<ArrayList<Jednostki>> mapa;
+    static ArrayList<ArrayList<Jednostki>> mapa;
+    Armia armia1;
+    Armia armia2;
+
     Mapa(ArrayList<ArrayList<Jednostki>> mapa) {
         this.mapa = mapa;
     }
@@ -25,64 +29,105 @@ class Mapa {
         }
         System.out.println();
     }
-    public void stworzArmie(Armia armia){
-        nazwa=armia.toString();
-        kolumny= armia.kolumny();
-        rzedy= armia.rzedy();
-        jednostkiKolumny=armia.jednostkiKolumny();
-        if (nr==1){
-            x=kolumny+3;
-            for (int j=0; j<kolumny; j++){
-                for (int i=0; i<rzedy; i++){
-                    if(j==0){
+
+    public void stworzArmie(Armia armia) {
+        nazwa = armia.toString();
+        kolumny = armia.kolumny();
+        rzedy = armia.rzedy();
+        jednostkiKolumny = armia.jednostkiKolumny();
+        if (nr == 1) {
+            armia1 = armia;
+            x = kolumny + 3;
+            for (int j = 0; j < kolumny; j++) {
+                for (int i = 0; i < rzedy; i++) {
+                    if (j == 0) {
                         mapa.add(new ArrayList<Jednostki>());
                     }
-                    switch (jednostkiKolumny[j]){
+                    switch (jednostkiKolumny[j]) {
                         case piechota:
-                            mapa.get(i).add(new Piechota(nazwa,mapa,i,j, armia.liczebnoscTablica()));
+                            mapa.get(i).add(new Piechota(nazwa, mapa, i, j, armia.liczebnoscTablica()));
                             break;
                         case lekkaPiechota:
-                            mapa.get(i).add(new LekkaPiechota(nazwa,mapa,i,j, armia.liczebnoscTablica()));
+                            mapa.get(i).add(new LekkaPiechota(nazwa, mapa, i, j, armia.liczebnoscTablica()));
                             break;
                     }
                 }
             }
-            for (int i=0;i<rzedy;i++){
+            for (int i = 0; i < rzedy; i++) {
                 mapa.get(i).add(null);
                 mapa.get(i).add(null);
                 mapa.get(i).add(null);
             }
-        }
-        else if (nr==2){
-            if (mapa.size()<rzedy){
-                for(int i=mapa.size(); i<=rzedy-1; i++) {
+        } else if (nr == 2) {
+            armia2 = armia;
+            if (mapa.size() < rzedy) {
+                for (int i = mapa.size(); i <= rzedy - 1; i++) {
                     mapa.add(new ArrayList<Jednostki>());
-                    for(int j=0; j<x; j++)
+                    for (int j = 0; j < x; j++)
                         mapa.get(i).add(null);
                 }
             }
-            for (int j=0; j<kolumny; j++){
-                for (int i=0; i<rzedy;i++){
+            for (int j = 0; j < kolumny; j++) {
+                for (int i = 0; i < rzedy; i++) {
 
-                    switch (jednostkiKolumny[j]){
+                    switch (jednostkiKolumny[j]) {
                         case piechota:
-                            mapa.get(i).add(new Piechota(nazwa,mapa,i,j+x, armia.liczebnoscTablica()));
+                            mapa.get(i).add(new Piechota(nazwa, mapa, i, j + x, armia.liczebnoscTablica()));
                             break;
                         case lekkaPiechota:
-                            mapa.get(i).add(new LekkaPiechota(nazwa,mapa,i,j+x, armia.liczebnoscTablica()));
+                            mapa.get(i).add(new LekkaPiechota(nazwa, mapa, i, j + x, armia.liczebnoscTablica()));
                             break;
                     }
                 }
             }
-            if (mapa.size()>rzedy){
-                for (int i=rzedy; i<mapa.size();i++){
-                    for (int j=0;j<kolumny;j++)
+            if (mapa.size() > rzedy) {
+                for (int i = rzedy; i < mapa.size(); i++) {
+                    for (int j = 0; j < kolumny; j++)
                         mapa.get(i).add(null);
                 }
             }
-        }
-        else
+        } else
             System.out.println("Blad tworzenia armii!!!");
         nr++;
     }
-}
+
+    public void zapisTury(int tura) throws IOException {
+        File zapis = new File("zapis.txt");
+        FileWriter writer = new FileWriter(zapis, true);
+        writer.write("\n");
+        writer.write("TURA " + tura + ".    " + "LICZEBNOSC: " + armia1 + " " + armia1.liczebnosc() + ", " + armia2 + " " + armia2.liczebnosc());
+        writer.close();
+    }
+
+    public void zapisKoniec(int tura) throws IOException {
+        File zapis = new File("zapis.txt");
+        FileWriter writer = new FileWriter(zapis, true);
+        writer.write("\n");
+        writer.write("KONIEC");
+        writer.write("\n");
+        writer.write("ZAKONCZONO PO TURZE "+tura+".   REZULTAT: "+armia1+" "+armia1.liczebnosc()+"; "+armia2+" "+armia2.liczebnosc());
+
+        writer.close();
+    }
+
+
+    public void zapis() throws IOException {
+        File zapis = new File("zapis.txt");
+        FileWriter writer = new FileWriter(zapis, true);
+        writer.write("\n");
+        for (int i = 0; i < mapa.size(); i++) {
+            for (int j = 0; j < mapa.get(0).size(); j++) {
+                if (mapa.get(i).get(j) != null)
+                    writer.write(mapa.get(i).get(j).toString());
+                else
+                    writer.write(" ");
+            }
+            writer.write("\n");
+        }
+        writer.write("---------------------------");
+        writer.close();
+    }
+    }
+
+
+
